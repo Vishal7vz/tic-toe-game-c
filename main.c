e0b1749acc02e30d115881dc723233bc509e7c2f
vishal singh <vishalsingh7vz@gmail.com>
2025-02-24 12:53:27 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,76 @@
 #include <stdio.h>
 
-int main() {
-    int n, i;
+char board[3][3];
 
-    // Taking user input
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-
-    // Printing the multiplication table
-    printf("Multiplication table of %d:\n", n);
-    for (i = 1; i <= 10; i++) {
-        printf("%d x %d = %d\n", n, i, n * i);
+void initializeBoard() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ' ';
+        }
     }
+}
 
+void printBoard() {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < 2) printf("|");
+        }
+        printf("\n");
+        if (i < 2) printf("---|---|---\n");
+    }
+    printf("\n");
+}
+
+int checkWin() {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') return 1;
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') return 1;
+    }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') return 1;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') return 1;
+    return 0;
+}
+
+int isDraw() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ') return 0;
+        }
+    }
+    return 1;
+}
+
+void playGame() {
+    int row, col, player = 1;
+    char mark;
+    initializeBoard();
+    while (1) {
+        printBoard();
+        mark = (player == 1) ? 'X' : 'O';
+        printf("Player %d, enter row and column (0-2): ", player);
+        scanf("%d %d", &row, &col);
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+            board[row][col] = mark;
+            if (checkWin()) {
+                printBoard();
+                printf("Player %d wins!\n", player);
+                return;
+            }
+            if (isDraw()) {
+                printBoard();
+                printf("It's a draw!\n");
+                return;
+            }
+            player = (player == 1) ? 2 : 1;
+        } else {
+            printf("Invalid move. Try again.\n");
+        }
+    }
+}
+
+int main() {
+    playGame();
     return 0;
 }
